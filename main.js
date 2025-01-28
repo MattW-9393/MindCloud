f5b0f6eff00e08dcb6a17781ece7564deb1d64ca
mattw-9393 <matt.warne22@gmail.com>
2025-01-28 09:54:23 +0000

    most up to date css and html including centre align form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+// Variables for Sign in and Create Account Forms
+let signInForm = document.getElementById('sign-in-form')
+let createAccountForm = document.getElementById('create-acc-form').hidden = true;
+
+
+// Hidden items
+
+signInForm.hidden = true;
+createAccountForm.hidden = true;
+
+
+// Reveal forms functions
+
