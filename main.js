02511b221c359d59d4e9d3beb235ee7e207ce9c5
Matt <81921484+MattW-9393@users.noreply.github.com>
2025-03-03 21:16:09 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Variables for Sign in and Create Account Forms
 let signInForm = document.getElementById('sign-in-form')
-let createAccountForm = document.getElementById('create-acc-form').hidden = true;
+let createAccountForm = document.getElementById('create-acc-form')
 
 
 // Hidden items
