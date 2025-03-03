208b95b5b17fe3747a80746b0c9334f4599ea978
mattw-9393 <matt.warne22@gmail.com>
2025-03-03 21:51:49 +0000

    modified:   main.js

--- main.js
+++ main.js
@@ -2,13 +2,27 @@
 let signInForm = document.getElementById('sign-in-form')
 let createAccountForm = document.getElementById('create-acc-form')
 
+let haveAccount = document.getElementById('i-have-acc')
+let newAccountPlz = document.getElementById('create-acc-plz')
+
 
 // Hidden items
 
+//Welcome section - hidden items
 signInForm.hidden = true;
 createAccountForm.hidden = true;
 
 
-// Reveal forms functions
+// Reveal sign in and create account forms functions - Reformat anonymous functions to event handler functions
+
+haveAccount.onclick = function(){
+    signInForm.hidden = false;
+    createAccountForm.hidden = true;
+}
+
+newAccountPlz.onclick = function() {
+    createAccountForm.hidden= false;
+    signInForm.hidden=true;
+}
 
 
